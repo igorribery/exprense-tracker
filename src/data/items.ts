c84395d9374fd0c1dcd93f91a83d12a7805b720d
Igor <(igorrds1998@gmail.com)>
2023-01-14 00:18:44 -0300

    adding files to repo

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    { date: new Date(2023, 0, 13), category: 'food', title: 'McDonals', value: 32.12 },
+    { date: new Date(2023, 0, 13), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2023, 0, 13), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2023, 0o1, 13), category: 'salary', title: 'Salário', value: 4500 },
+];
